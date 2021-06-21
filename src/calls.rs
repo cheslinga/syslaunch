01476b1ac4e026b6ec202655b7446463a7bda4de
Cole Heslinga <cole.heslinga@gmail.com>
2021-06-21 17:25:27 -0400

    FS mount errors are now individually printed.

--- src/calls.rs
+++ src/calls.rs
@@ -2,7 +2,6 @@ use nix::sys::reboot::*;
 use nix::unistd::*;
 use std::ffi::CString;
 use nix::mount::{mount, MsFlags};
-use nix::sys::socket::MsgFlags;
 
 pub fn p_shutdown() {
     println!("Init received signal for shutdown...");
@@ -26,18 +25,13 @@ pub fn exec_process(path: &CString, args: &Vec<CString>, env: &Vec<CString>) {
 }
 
 pub fn mount_dirs() {
-    let mut errors = String::new();
 
-    if let Some(_) = mount::<str, str, str, str>(Some("proc"), "/proc", Some("proc"),
-                           MsFlags::MS_NOEXEC | MsFlags::MS_NOSUID | MsFlags::MS_NODEV, None).err() { errors.push_str("proc ") }
-    if let Some(_) = mount::<str, str, str, str>(Some("sys"), "/sys", Some("sysfs"),
-                           MsFlags::MS_NOEXEC | MsFlags::MS_NOSUID | MsFlags::MS_NODEV, None).err() { errors.push_str("sys ") }
-    if let Some(_) = mount::<str, str, str, str>(Some("run"), "/run", Some("tmpfs"),
-                           MsFlags::MS_NOSUID | MsFlags::MS_NODEV, Some("mode=0755")).err() { errors.push_str("run ") }
-    if let Some(_) = mount::<str, str, str, str>(Some("dev"), "/dev", Some("devtmpfs"),
-                           MsFlags::MS_NOSUID, Some("mode=0755")).err() { errors.push_str("dev ") }
-
-    if !errors.is_empty() {
-        eprintln!("|!!| Unable to mount the following filesystems:\n|!!| {}", errors);
-    }
+    if let Some(e) = mount::<str, str, str, str>(Some("proc"), "/proc", Some("proc"),
+                           MsFlags::MS_NOEXEC | MsFlags::MS_NOSUID | MsFlags::MS_NODEV, None).err() { eprintln!("|!!| Error mounting proc: {}", e) }
+    if let Some(e) = mount::<str, str, str, str>(Some("sys"), "/sys", Some("sysfs"),
+                           MsFlags::MS_NOEXEC | MsFlags::MS_NOSUID | MsFlags::MS_NODEV, None).err() { eprintln!("|!!| Error mounting sysfs: {}", e) }
+    if let Some(e) = mount::<str, str, str, str>(Some("run"), "/run", Some("tmpfs"),
+                           MsFlags::MS_NOSUID | MsFlags::MS_NODEV, Some("mode=0755")).err() { eprintln!("|!!| Error mounting tmpfs: {}", e) }
+    if let Some(e) = mount::<str, str, str, str>(Some("dev"), "/dev", Some("devtmpfs"),
+                           MsFlags::MS_NOSUID, Some("mode=0755")).err() { eprintln!("|!!| Error mounting devtmpfs: {}", e) }
 }
